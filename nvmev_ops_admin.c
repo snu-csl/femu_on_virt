0cf340611b75aff24b2f90fcd4b53ea05ec8db9a
Sang-Hoon Kim <sanghoonkim@ajou.ac.kr>
2020-10-20 15:54:02 +0900

    Added copyright headers

--- nvmev_ops_admin.c
+++ nvmev_ops_admin.c
@@ -1,3 +1,17 @@
+/**********************************************************************
+ * Copyright (c) 2020
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTIABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ **********************************************************************/
+
 #include "nvmev.h"
 
 #define sq_entry(entry_id) \
